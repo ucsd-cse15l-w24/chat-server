0a41e4ddc3eca8daeb0b89406b1f1c42c26927c4
MohaElder <calen0909@hotmail.com>
2024-02-27 14:51:06 -0800

    fix: correct class nesting

--- ChatServer.java
+++ ChatServer.java
@@ -103,11 +103,11 @@ class ChatHandler implements URLHandler {
     }
     return this.chatHistory;
   }
+}
 
-  class ChatServer {
-    public static void main(String[] args) throws IOException {
-      int port = Integer.parseInt(args[0]);
-      Server.start(port, new ChatHandler());
-    }
+class ChatServer {
+  public static void main(String[] args) throws IOException {
+    int port = Integer.parseInt(args[0]);
+    Server.start(port, new ChatHandler());
   }
 }
